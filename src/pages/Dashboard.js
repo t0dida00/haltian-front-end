628f9a317b18854f88cf51dbbec1a85cc11af3d8
Duong Pham <t0phtr00@students.oamk.fi>
2023-03-21 22:08:39 +0200

    Update

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,36 @@
 import React from "react"
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <>
+      <div>Air Quality Measure</div>
+      <div>
+        <div>Air Quality</div>
+        <div>
+          <div>Carbon Dioxide</div>
+          <div>T.V.O.C.</div>
+        </div>
+        <div>
+          <div>Air Pressure</div>
+          <div>Temperature</div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <div>Humidity</div>
+              <div>Light Level</div>
+            </div>
+            <div>
+              <div>Humidity</div>
+              <div>Light Level</div>
+            </div>
+          </div>
+          <div>Suggestion</div>
+        </div>
+        <div>Graph</div>
+      </div>
+    </>
+  )
 }
